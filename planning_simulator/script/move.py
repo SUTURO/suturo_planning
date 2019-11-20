13f36df75fb0cc182387cdbfcfdf34973c095b20
Skadir <janschimpf@web.de>
2019-11-20 14:41:08 +0100

    some small changes

---
+++ planning_simulator/script/move.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from hsrb_interface import Robot
+import rospy
+
+# Preparation to use robot functions
+robot = Robot()
+base = robot.try_get('omni_base')
+tts = robot.try_get('default_tts')
+whole_body = robot.try_get('whole_body')
+
+if __name__=='__main__':
+    # Move to initial posture
+    try:
+#	base.go_abs(0, -2, 0, 180.0)
+        base.go_abs(0.6, -0.2, 0, 180.0)
+        whole_body.move_to_joint_positions({'head_tilt_joint': -0.15})
+
+    except:
+        rospy.logerr('Fail move_to_neutral')
