ef17ee20c5301af3aaae877448a4b64ef5b56dec
torge <torge1995@hotmail.de>
2019-11-21 21:16:45 +0100

    added perception trigger to move.py added move.py to launchfile with package timed_launchfile added srv_client for future stuff

--- planning_simulator/scripts/move.py
+++ planning_simulator/scripts/move.py
@@ -3,6 +3,7 @@
 from hsrb_interface import Robot
 import rospy
 import math
+from std_srvs.srv import Trigger, TriggerRequest
 
 rospy.init_node('move', anonymous=True)
 
@@ -18,6 +19,15 @@ if __name__=='__main__':
 	base.go_abs(1.0, -2.0, -1.0, 180.0)
 #        base.go_abs(0.6, -0.2, 0, 180.0)	
         whole_body.move_to_joint_positions({'head_tilt_joint': -0.3})
-
     except:
         rospy.logerr('Fail move_to_neutral')
+    rospy.wait_for_service('/perception_pipeline/trigger')
+    srv = rospy.ServiceProxy('/perception_pipeline/trigger',Trigger)
+    try:
+       msg = TriggerRequest()
+       result = srv(msg)
+       print result;
+    except rospy.ServiceException as exc:
+	print "Service call to perception_pipeline failed.";
+    
+    
