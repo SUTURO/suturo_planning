ffc3195040ffb4ee43488176a5103a60a4a98169
torge <torge1995@hotmail.de>
2019-12-14 16:14:13 +0100

    fixed perception pipeline client

--- planning_milestone/low_level_interfacing/src/perception-trigger-client.lisp
+++ planning_milestone/low_level_interfacing/src/perception-trigger-client.lisp
@@ -1,9 +1,9 @@
-;;(in-package : llif)
+(in-package :llif)
 
-;;(defun trigger-perception-pipeline()
-;;  "Triggers perception pipeline"
-;;  (with-ros-node ("perception_triggerer")
-;;    (if (not (wait-for-service ("perception_pipeline") 10))
-;;	(ros-warn nil "Timed out waiting for service perception_pipeline")
-;;      (if (call-service "perception_pipeline" 'TriggerRequest))
-;;        (ros-info nil "Perception Pipeline Triggered: True" )))
+(defun trigger-perception-pipeline-main()
+  "Trigger perception pipeline"
+  (with-ros-node ("perception_client")
+		 (if (not (wait-for-service "perception_pipeline" 10))
+		     (ros-warn nil "Timed out out waiing for service perception_pipeline")))
+		   (if (call-service "perception_pipeline" 'TriggerRequest)
+		       (ros-info nil "Perception pipeline was triggered...")))
