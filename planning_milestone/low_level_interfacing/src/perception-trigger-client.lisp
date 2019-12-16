6fbaef8a6e531c3550d16b43b3786a8a87d543b6
torge <torge1995@hotmail.de>
2019-12-16 12:49:38 +0100

    fixed perception trigger client?

--- planning_milestone/low_level_interfacing/src/perception-trigger-client.lisp
+++ planning_milestone/low_level_interfacing/src/perception-trigger-client.lisp
@@ -4,6 +4,6 @@
   "Trigger perception pipeline"
   (with-ros-node ("perception_client")
 		 (if (not (wait-for-service "perception_pipeline" 120))
-		     (ros-warn nil "Timed out out waiing for service perception_pipeline")))
+		     (ros-warn nil "Timed out out waiing for service perception_pipeline")
 		   (if (call-service "perception_pipeline" 'TriggerRequest)
-		       (ros-info nil "Perception pipeline was triggered...")))
+		       (ros-info nil "Perception pipeline was triggered...")))))
