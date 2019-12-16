f4f7eafc4c3ddda7b5256200803bd7a518eba606
torge <torge1995@hotmail.de>
2019-12-16 12:18:25 +0100

    added emacs config so you can pull it changed timeout in perception trigger client

--- planning_milestone/low_level_interfacing/src/perception-trigger-client.lisp
+++ planning_milestone/low_level_interfacing/src/perception-trigger-client.lisp
@@ -3,7 +3,7 @@
 (defun trigger-perception-pipeline-main()
   "Trigger perception pipeline"
   (with-ros-node ("perception_client")
-		 (if (not (wait-for-service "perception_pipeline" 10))
+		 (if (not (wait-for-service "perception_pipeline" 120))
 		     (ros-warn nil "Timed out out waiing for service perception_pipeline")))
 		   (if (call-service "perception_pipeline" 'TriggerRequest)
 		       (ros-info nil "Perception pipeline was triggered...")))
