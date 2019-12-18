d2e1e57d46e5a11a7274edfb827ed09e57bd95a4
Skadir <janschimpf@web.de>
2019-12-18 13:58:28 +0100

    start to fill high-level

--- planning_milestone/common_plans/src/high-level.lisp
+++ planning_milestone/common_plans/src/high-level.lisp
@@ -1,11 +1,11 @@
 (in-package :comp)
 
 (defun move-to-table()
-
+  (llif::call-nav-action 0 0 0)
 )
 
 (defun find-object()
-
+(call-robosherlock-pipeline)
 )
 
 (defun grasp-object()
@@ -13,9 +13,17 @@
 )
 
 (defun move-to-goal()
-
+ (llif::call-nav-action 0 0 0)
 )
 
 (defun place-object()
 
+)
+
+(defun execute-m1()
+       (move-totable)
+       (find-object)
+       (grasp-object)
+       (move-to-goal)
+       (place-object)
 )
