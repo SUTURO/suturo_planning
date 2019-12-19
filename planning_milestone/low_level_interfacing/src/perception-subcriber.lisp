2f6e54f33a7d6d807e8cb6222d4e2eb194b6f90e
Skadir <janschimpf@web.de>
2019-12-19 12:45:14 +0100

    perceptionsubcriber works

--- planning_milestone/low_level_interfacing/src/perception-subcriber.lisp
+++ planning_milestone/low_level_interfacing/src/perception-subcriber.lisp
@@ -2,4 +2,4 @@
 
 (defun listener ()
   (with-ros-node ("listener" :spin t)
-    (subscribe "/perception_pipeline/result_advertiser" "RSObjectDescriptions" #'print)))
+    (subscribe "/perception_pipeline/result_advertiser" "perception_msgs/RSObjectDescriptions" #'print)))
