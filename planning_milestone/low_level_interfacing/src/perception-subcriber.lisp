757d2c3c315c3a467aa3905a5d926f5a292172f9
Skadir <janschimpf@web.de>
2019-12-19 13:22:28 +0100

    changed the subscriber over to the parsed topic

--- planning_milestone/low_level_interfacing/src/perception-subcriber.lisp
+++ planning_milestone/low_level_interfacing/src/perception-subcriber.lisp
@@ -2,4 +2,4 @@
 
 (defun listener ()
   (with-ros-node ("listener" :spin t)
-    (subscribe "/perception_pipeline/result_advertiser" "perception_msgs/RSObjectDescriptions" #'print)))
+    (subscribe "/perception_output/planning" "std_msgs/String" #'print)))
