c0a37c4fe02b69984ca0321b0a9f6a219609b2e9
Skadir <janschimpf@web.de>
2019-12-19 17:25:08 +0100

    update to the perception-subscriber

--- planning_milestone/low_level_interfacing/src/perception-subcriber.lisp
+++ planning_milestone/low_level_interfacing/src/perception-subcriber.lisp
@@ -1,5 +1,20 @@
 (in-package :llif)
 
+(defvar *color* 'red)
+
 (defun listener ()
   (with-ros-node ("listener" :spin t)
-    (subscribe "/perception_output/planning" "std_msgs/String" #'print)))
+    (subscribe "/perception_output/planning"  "perception_msgs/RSObject" #'sorting)))
+
+(defun sorting(msg)
+ (defvar *px* (roslisp:msg-slot-value msg :x))
+ (roslisp:ros-info (init-clients)"*px*")
+ (defvar *py* (roslisp:msg-slot-value msg :y))
+ (defvar *pz* (roslisp:msg-slot-value msg :z))
+ (defvar *pw* (roslisp:msg-slot-value msg :w))
+ (defvar *ph* (roslisp:msg-slot-value msg :h))
+ (defvar *pd* (roslisp:msg-slot-value msg :d))
+ (defvar *confidence* (roslisp:msg-slot-value msg :confidence))
+ (defvar *color* (roslisp:msg-slot-value msg :color_name))
+
+)
