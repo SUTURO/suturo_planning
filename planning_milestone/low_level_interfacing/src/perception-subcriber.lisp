53b3db4b5eebf0ebb89fafa9a17a7d34b9315fe0
Philipp <phklein@uni-bremen.de>
2019-12-20 14:27:04 +0100

    listener fix

--- planning_milestone/low_level_interfacing/src/perception-subcriber.lisp
+++ planning_milestone/low_level_interfacing/src/perception-subcriber.lisp
@@ -1,6 +1,7 @@
 (in-package :llif)
 
 (defvar *color* "red")
+(defvar *check* T)
 
 (defun listener ()
   ;;(with-ros-node ("listener" :spin t)
@@ -17,8 +18,11 @@
         (*confidence* (roslisp:msg-slot-value msg :confidence))
         (*color-of-object* (roslisp:msg-slot-value msg :color_name)))
 
-    (roslisp:ros-info (sorting) "~a'." *px*) 
+    (roslisp:ros-info (sorting) "TETSTTSTTAS DATSD ~a'." *px*) 
 
     (if (equalp *color-of-object* *color*)
-        (call-grasp-action *px* *py* 0.28 0.0 0.7 0.0 0.7 *pw* *ph* *pd*)
-)))
+        (if (eq *check* T)
+            (progn
+              (call-grasp-action *px* *py* 0.28 0.0 0.7 0.0 0.7 *pw* *ph* *pd*)
+              (setf *check* nil)
+)))))
