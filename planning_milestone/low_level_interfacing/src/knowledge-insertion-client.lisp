71382af7dbca96958be955776168dc3e5de62954
tomlehmk <tomlehmk@uni-bremen.de>
2020-01-21 13:58:02 +0100

    knowledge-insert-test function

--- planning_milestone/low_level_interfacing/src/knowledge-insertion-client.lisp
+++ planning_milestone/low_level_interfacing/src/knowledge-insertion-client.lisp
@@ -32,3 +32,28 @@
                        :timeout *robosherlock-action-timeout*
                        :result-timeout *robosherlock-action-timeout*)))
 
+(defun knowledge-insertion-test ()
+(defparameter *position* (roslisp:make-msg "geometry_msgs/Point" (x) 2 (y) 4 (z) 0))
+(defparameter *orientation* (roslisp:make-msg "geometry_msgs/Quaternion" (x) 0 (y) 1 (z) 0 (w) 1))
+(defparameter *pose* (roslisp:make-msg "geometry_msgs/Pose" (position) *position* (orientation) *orientation*))
+(defparameter *header* (roslisp:make-msg "std_msgs/Header" (frame_id) "/frame"))
+  (defparameter *object-message* (roslisp:make-msg "suturo_perception_msgs/ObjectDetectionData"
+    (name) "test-object"
+    (obj_class) "test-ckass"
+    (confidence_class) 1.0
+    (shape) 1
+    (confidence_shape) 0.8
+    (pose) (roslisp:make-msg "geometry_msgs/PoseStamped"
+      (header) *header*
+      (pose) *pose*)
+    (width) 1.3
+    (height) 5.7
+    (depth) 0.2
+    (color) (roslisp:make-msg "std_msgs/ColorRGBA"
+      (r) 1
+      (g) 1
+      (b) 0
+      (a) 0)
+    (confidence_color) 0.6
+    (region) "somewhere"))
+    (insert-knowledge-objects *object-message*))
