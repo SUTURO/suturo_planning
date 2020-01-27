48fe74c5375fd04e9b02edb97f3be97ff9bdcbc3
Skadir <janschimpf@web.de>
2020-01-27 11:10:28 +0100

    small changes to the init
    added the knowledge init and dealt with a double init of a node.

--- common_plans/src/init.lisp
+++ common_plans/src/init.lisp
@@ -23,8 +23,14 @@
   (roslisp:ros-info (init-clients) "init robosherlock object action client")
   (llif::init-robosherlock-object-action-client)
 
+  (roslisp:ros-info (init-clients) "init robosherlock object action client")
+  (llif::init-robosherlock-object-action-client)
+
   (roslisp:ros-info (init-clients) "init robosherlock plane action client")
   (llif::init-robosherlock-plane-action-client)
+ 
+  (roslisp:ros-info (init-clients) "init knowledge action client")
+  (llif:::init-knowledge-action-client)
 )
 
 (defun init-navigation()
@@ -42,6 +48,7 @@
 (defun init-manipulation()
  "Initialize only local nodes for working without the real robot."
   (print "create ros node")
+
   (roslisp-utilities:startup-ros :name "planning_node" :anonymous nil)
   (print "init tf-listener")
   (comp::get-tf-listener)
