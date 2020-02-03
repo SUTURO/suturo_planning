7c8fb74c837beebe2475721d5b5392deaef838ab
Skadir <janschimpf@web.de>
2020-02-03 12:07:49 +0100

    Did a first version for place object
    untested for now, compiles, still has no failure handling

--- common_plans/common-plans.asd
+++ common_plans/common-plans.asd
@@ -22,6 +22,7 @@
              (:file "utils" :depends-on ("package"))
              (:file "high-level" :depends-on("package"))
 	     (:file "next-object" :depends-on("package"))   
-             (:file "grasp-object" :depends-on("package")) 
+             (:file "grasp-object" :depends-on("package"))
+             (:file "place-object" :depends-on("package")) 
 	     (:file "poiScan" :depends-on("package"))  
 ))))
---
+++ common_plans/src/place-object.lisp
@@ -0,0 +1,16 @@
+(in-package :comp)
+(defvar *goal*)
+
+(defun grasp-object (object-id)
+  (setq *goal* (llif:prolog-object-goal-pose object-id))
+
+  (let ((px (nth 0 (nth 2 *pose*)))
+        (py (nth 1 (nth 2 *pose*)))
+        (pz (nth 2 (nth 2 *pose*)))
+        (qv1 (nth 0 (nth 3 *pose*)))
+        (qv2 (nth 1 (nth 3 *pose*)))
+        (qv3 (nth 2 (nth 3 *pose*)))
+        (qv4 (nth 3 (nth 3 *pose*))))
+  (llif:call-place-action px py pz qv1 qv2 qv3 qv4 object-id)
+  ))
+ 
