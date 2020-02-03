8164739f3b4c56d0bb2212bffb319895e5c2ba6f
Skadir <janschimpf@web.de>
2020-02-03 14:30:04 +0100

    place-object

--- common_plans/src/place-object.lisp
+++ common_plans/src/place-object.lisp
@@ -7,13 +7,13 @@
   (setq *class* (llif:object-name->class object-id))
   ;;say: place object of class *class* at *goal*
 
-  (let ((px (nth 0 (nth 2 *pose*)))
-        (py (nth 1 (nth 2 *pose*)))
-        (pz (nth 2 (nth 2 *pose*)))
-        (qv1 (nth 0 (nth 3 *pose*)))
-        (qv2 (nth 1 (nth 3 *pose*)))
-        (qv3 (nth 2 (nth 3 *pose*)))
-        (qv4 (nth 3 (nth 3 *pose*))))
+  (let ((px (nth 0 (nth 0 *pose*)))
+        (py (nth 1 (nth 0 *pose*)))
+        (pz (nth 2 (nth 0 *pose*)))
+        (qv1 (nth 0 (nth 1 *pose*)))
+        (qv2 (nth 1 (nth 1 *pose*)))
+        (qv3 (nth 2 (nth 1 *pose*)))
+        (qv4 (nth 3 (nth 1 *pose*))))
   (llif:call-place-action px py pz qv1 qv2 qv3 qv4 object-id)
     )
   ;;say: done placeing object
