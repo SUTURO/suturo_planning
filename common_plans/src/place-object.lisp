54afaa2e085a6e036fdddd674a5b2825181db6b7
Skadir <janschimpf@web.de>
2020-02-03 13:48:53 +0100

    small updates for grasp and place

--- common_plans/src/place-object.lisp
+++ common_plans/src/place-object.lisp
@@ -1,8 +1,11 @@
 (in-package :comp)
 (defvar *goal*)
-
+(defvar *class*)
+  
 (defun place-object (object-id)
   (setq *goal* (llif:prolog-object-goal-pose object-id))
+  (setq *class* (llif:object-name->class object-id))
+  ;;say: place object of class *class* at *goal*
 
   (let ((px (nth 0 (nth 2 *pose*)))
         (py (nth 1 (nth 2 *pose*)))
@@ -12,5 +15,7 @@
         (qv3 (nth 2 (nth 3 *pose*)))
         (qv4 (nth 3 (nth 3 *pose*))))
   (llif:call-place-action px py pz qv1 qv2 qv3 qv4 object-id)
-  ))
+    )
+  ;;say: done placeing object
+  )
  
