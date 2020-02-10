69054a2d76b4eb392664a009de19d41640c73106
Skadir <janschimpf@web.de>
2020-02-10 10:56:15 +0100

    started working on the urdf-proj for grasping

--- common_plans/src/high-level-plans.lisp
+++ common_plans/src/high-level-plans.lisp
@@ -1,5 +1,6 @@
 (in-package :comp)
 
+;;;; Navigation ;;;;
 (defun try-movement () (let 
 	((?nav-pose (list (cl-tf::make-pose-stamped "map" 0 
                                                (cl-tf:make-3d-vector 2 3 0) 
@@ -37,3 +38,36 @@
 (comp::with-hsr-process-modules (exe:perform (desig:a motion (type going) 
 (target (desig:a location (pose ?successfull-pose))))))
 ))
+
+;;;; Grasp ;;;;
+(defun try-grasp () (let 
+                        ((?grasp (list (1 1 0.5 0 0.7 0 0.7 0.1 0.1 0.1 "box" 1)
+                                       (1 1 0.5 0 0.7 0 0.7 0.1 0.1 0.1 "box" 1)
+                                       (1 1 0.5 0 0.7 0 0.7 0.1 0.1 0.1 "box" 1))))
+
+;;(urdf-proj:with-simulated-robot (cpl:with-retry-counters ((going-retry 3))
+;;      (cpl:with-failure-handling
+;;          (((or common-fail:low-level-failure 
+;;                cl::simple-error
+;;                cl::simple-type-error)                
+;;               (e)
+;;             (setf ?grasp (cdr ?grasp))
+;;             (cpl:do-retry going-retry
+;;               (roslisp:ros-warn (going-demo movement-fail)
+;;                                 "~%Failed to grasp object~%")
+;;               (cpl:retry))
+;;             (roslisp:ros-warn (going-demo movement-fail)
+;;                               "~%No more retries~%")))
+;;          (let ((?actual-nav-pose (car ?nav-pose))) 
+;;          (cram-executive:perform
+;;           (desig:an action
+;;                     (type grasping)
+;;                     (target (desig:a location (pose ?actual-nav-pose)))))
+;;            ?actual-nav-pose))))))
+
+;;(defun grasp-hsr ()
+;;  (let ((?successfull-pose (try-movement)))
+;;(comp::with-hsr-process-modules (exe:perform (desig:a motion (type grasping) 
+;;                                                      (target (desig:a location (pose ?successfull-pose))))))
+
+));;;; Place ;;;;
