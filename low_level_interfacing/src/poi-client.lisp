4d453fd15375a233e64949b2bb6a21121e2421e4
Philipp <phklein@uni-bremen.de>
2020-02-18 14:08:18 +0100

    subscriber zu POI funktioniert

--- low_level_interfacing/src/poi-client.lisp
+++ low_level_interfacing/src/poi-client.lisp
@@ -11,8 +11,12 @@
 (defparameter *poi* (list
         ()))
 
-(defun popPoi () 
-     (pop *poi*))
+;; Mit BulletWorld eine geeignete Position neben dem punkt finden
+;;(defun nearestPoi (point) 
+     ;;(mapcar 
+     ;;    (lambda (arg) 
+     ;;        (list (cl-tf::v-dist (cl-tf::origin point) (cl-tf::origin arg)) (arg))
+     ;;)*poi*))
 
 (defun add-poi (px py pz ox oy oz ow)
 	(defparameter *poi* (append *poi* (list(make-poi px py pz ox oy oz ow))))
@@ -20,19 +24,27 @@
 )
 
 (defun add-stamped-poi (stamped)
-	(defparameter *poi* (append *poi* (list(stamped))))
+	(defparameter *poi* (append *poi* (list stamped)))
 )
 
 
 
 (defun poi-listener ()
   (with-ros-node ("listener" :spin t)
-    (subscribe "object_finder" "geometry_msgs/PoseArray" #'addPoiFromTopic)))
+    (subscribe "object_finder" "geometry_msgs/PoseArray" #'addPoiFromTopic))
+  (roslisp:ros-info (poi-subscriber) "POI Subscriber started")
+)
 
 (defun addPoiFromTopic (poseArrayMsg) 
-        (roslisp:ros-info (poi-subscriber) "added POIs to List")
+  ;;(roslisp:ros-info (poi-subscriber) "added POIs to List")
+  (defparameter *poi* (list()))
+    (roslisp:with-fields (poses) poseArrayMsg
         (mapcar 
-          (lambda (arg) (add-stamped-poi(cl-tf::pose->pose-stamped "map" 0.0 arg)))
-          (roslisp::with-fields (poses) poseArrayMsg)
+            (lambda (arg) 
+                         (add-stamped-poi(cl-tf::pose->pose-stamped "map" 0.0 (cl-tf::from-msg arg)))
+                         ;;(roslisp:ros-info (poi-subscriber) "added ~a"arg)
+                         ;;(roslisp:ros-info (poi-subscriber) "Die ganze Liste: ~a"*poi*)
+            )(coerce poses 'list)
         )
+    )
 )
