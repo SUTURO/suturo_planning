4c77f6fb57d57f246d0095c4e85a555a1081ab83
tolliges <torge1995@hotmail.de>
2020-02-19 13:00:50 +0100

    fixed some stuff refactored grocery and cleanup package

--- low_level_interfacing/src/process-modules.lisp
+++ low_level_interfacing/src/process-modules.lisp
@@ -3,7 +3,7 @@
 (defmacro with-hsr-process-modules (&body body)
   `(cram-process-modules:with-process-modules-running
        (llif::hsr-navigation)
-       (llif::hsr-arm)
+       ;;(llif::hsr-arm)
      (cpl-impl::named-top-level (:name :top-level)
        ,@body)))
 
@@ -15,52 +15,52 @@
   (destructuring-bind (command target) (desig:reference motion-designator)
     (ecase command
       (going
-       (llif::call-nav-action-ps (desig:reference target))
-       ))))
+       (llif::call-nav-action-ps (desig:reference target)))
+    )))
 
  ;;;;;;;;;;;;;;;;;;;; Arm  ;;;;;;;;;;;;;;;;;;;;;;;;
 
-(cram-process-modules:def-process-module hsr-arm (motion-designator)
-  (roslisp:ros-info (hsr-arm-process-modules)
-                    "hsr-arm called with a motion designator `~a'."
-                    motion-designator)
-  (destructuring-bind (command
-                       ?point-x
-                       ?point-y
-                       ?point-z
-                       ?quaterion-value-1
-                       ?quaterion-value-2
-                       ?quaterion-value-3
-                       ?quaterion-value-4
-                       ?size_x
-                       ?size_y
-                       ?size_z
-                       ?object_id
-                       ?graspmode)
-      (desig:reference motion-designator)
-    (ecase command
-      (grasping
-       (llif::call-grasp-action
-        ?point-x
-        ?point-y
-        ?point-z
-        ?quaterion-value-1
-        ?quaterion-value-2
-        ?quaterion-value-3
-        ?quaterion-value-4
-        ?size_x
-        ?size_y
-        ?size_z
-        ?object_id
-        ?graspmode))
-      (placeing
-       (llif::call-place-action
-        ?point-x
-        ?point-y
-        ?point-z
-        ?quaterion-value-1
-        ?quaterion-value-2
-        ?quaterion-value-3
-        ?quaterion-value-4
-        ?object_id
-        ?graspmode)))))
+;;(cram-process-modules:def-process-module hsr-arm (motion-designator)
+;;  (roslisp:ros-info (hsr-arm-process-modules)
+;;                    "hsr-arm called with a motion designator `~a'."
+;;                    motion-designator)
+;;  (destructuring-bind (command
+;;                       ?point-x
+;;                       ?point-y
+;;                       ?point-z
+;;                       ?quaterion-value-1
+;;                       ?quaterion-value-2
+;;                       ?quaterion-value-3
+;;                       ?quaterion-value-4
+;;                       ?size_x
+;;                       ?size_y
+;;                       ?size_z
+;;                       ?object_id
+;;                       ?graspmode)
+;;      (desig:reference motion-designator)
+;;    (ecase command
+;;      (grasping
+;;       (llif::call-grasp-action
+;;        ?point-x
+;;        ?point-y
+;;        ?point-z
+;;        ?quaterion-value-1
+;;        ?quaterion-value-2
+;;        ?quaterion-value-3
+;;        ?quaterion-value-4
+;;        ?size_x
+;;        ?size_y
+;;        ?size_z
+;;        ?object_id
+;;        ?graspmode))
+;;      (placeing
+;;       (llif::call-place-action
+;;        ?point-x
+;;        ?point-y
+;;        ?point-z
+;;        ?quaterion-value-1
+;;        ?quaterion-value-2
+;;        ?quaterion-value-3
+;;        ?quaterion-value-4
+;;        ?object_id
+;;        ?graspmode)))))
