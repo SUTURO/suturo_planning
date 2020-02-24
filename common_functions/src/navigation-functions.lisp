4d41576193f8160d92fdd6d83b215c6c5122c923
Philipp <phklein@uni-bremen.de>
2020-02-24 13:39:41 +0100

    poi fix

--- common_functions/src/navigation-functions.lisp
+++ common_functions/src/navigation-functions.lisp
@@ -1,13 +1,17 @@
 (in-package :comf)
 
 (defparameter *poiDistance* 0.5)
+(defparameter *currentOrigin* Nil)
+(defparameter *goalOrigin* Nil)
+(defparameter *newgoalOrigin* Nil)
+(defparameter *orientation* Nil)
+(defparameter *newgoalstamped* Nil)
 
 ;;(cpl:def-cram-function move-to-poi ()
 (defun move-to-poi ()
         ;;Point to go is: goal + (poiDistance/distance)*(currentpose - goal)
         (setf *currentOrigin* (cl-tf::origin (cl-tf::transform-stamped->pose-stamped (cl-tf::lookup-transform  cram-tf::*transformer*  "map" "base_footprint"))))
         (setf *goalOrigin* (cl-tf::origin(llif::closestPoi (cl-tf::transform-stamped->pose-stamped (cl-tf::lookup-transform  cram-tf::*transformer*  "map" "base_footprint")))))
-        (roslisp:ros-info (poi-subscriber) "going to: ~a" *goalOrigin*)
 	(setf *newgoalOrigin* (cl-tf::v+ *goalOrigin* 
 		(cl-tf::v* 
 		  (cl-tf::v- *currentOrigin* *goalOrigin*) 
@@ -15,13 +19,17 @@
 
 	
 	(setf *orientation* (+ 1.57 (atan (/ (cl-tf::y *goalOrigin*) (cl-tf::x *goalOrigin*)))))
-
-	(llif::call-nav-action-ps(cl-tf:make-pose-stamped
+        (setf *newgoalstamped* (cl-tf:make-pose-stamped
 		                "map"
 		                (roslisp::ros-time)
 		                *newgoalOrigin*
 		                (cl-tf:euler->quaternion :ax 0.0 :ay 0.0 :az *orientation*)))
 
+        (roslisp:ros-info (poi-subscriber) "going to: ~a" *newgoalstamped*)
+
+	(llif::call-nav-action-ps *newgoalstamped*)
+
+	(llif::call-take-pose-action 2)
 	;;going designator call + rotation calculation
 	;;(llif::call-nav-action-ps)
 )
