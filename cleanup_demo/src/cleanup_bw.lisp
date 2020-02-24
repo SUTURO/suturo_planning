64e4b7ff1ced669ba1065df23844746831134b57
Torge <torge1995@hotmail.de>
2020-02-24 13:49:38 +0100

    some changes in perception-functions (works maybe?), edited cleanup_bw.lisp

--- cleanup_demo/src/cleanup_bw.lisp
+++ cleanup_demo/src/cleanup_bw.lisp
@@ -1,1 +1,9 @@
 (in-package :clean)
+
+;;@author Torge Olliges
+;;spawns detected objects in the bulletworld
+(defun spawn-btr-objects
+    (mapcar 
+        (lambda (object) 
+            ((if (roslisp::with-fields object) (detectiondata))) 
+    comf::get-confident-objects)))
--- common_functions/src/perception-functions.lisp
+++ common_functions/src/perception-functions.lisp
@@ -1,23 +1,17 @@
 (in-package :comf)
 
 (defvar *objects* NIL)
-;;(defvar *x* NIL)
-(defvar *object-list* NIL)
-(defvar *pipeline-msg* NIL)
 
-;;(defun get-confident-objects ()
-;;  (setf *objects* (llif:call-robosherlock-pipeline))
-;;  (let* (?confidence (roslisp:slot-value objects :confidence))
-;;    (?objects (delete-if (lambda (arg) (confidence)) objects))))
-
-(defun get-confident-objects ()
-  (setq *objects* (roslisp::ros-message-to-list (llif::call-robosherlock-pipeline))) 
-  (roslisp::list-to-ros-message (roslisp::with-fields (detectiondata) *objects* (delete-if (lambda (obj)
-   (roslisp::with-fields (confidence_class) obj (> confidence_class 0.4)) *objects*) *objects*))))
-
-;;(defun get-confident-objects (&optional regions-value)
-;;  (setf *pipeline-msg* (llif:call-robosherlock-pipeline regions-value))
-;;  (roslisp::with-fields (detectiondata) *pipeline-msg* (setf *object-list* detectiondata))
-;;  (roslisp::list-to-ros-message (delete-if (lambda (object)
-;;  (roslisp::with-fields (confidence_class) object (> confidence_class 0.55)) *object-list*)
-;;  *object-list*))))										      
+;;@author Torge Olliges
+(defun get-confident-objects (&optional (regions-value (string "robocup_table")))
+  (setf *objects* (llif::call-robosherlock-pipeline regions-value))
+   (roslisp-msg-protocol:list-to-ros-message
+   (delete-if 
+    (lambda (obj)
+     (roslisp::with-fields 
+      (confidence_class) obj 
+      (if (> confidence_class 0.6) 
+          (print confidence_class)
+          (print confidence_class))))
+      *objects*))
+    *objects*))
