ac2702d067e1db2e995207a051242e25b08b1431
tolliges <torge1995@hotmail.de>
2020-03-03 20:04:50 +0100

    initial state for grocery execute

--- grocery_demo/src/execute-grocery.lisp
+++ grocery_demo/src/execute-grocery.lisp
@@ -1,35 +1,80 @@
 (in-package :grocery)
-(defvar *perceptionData*)
-(defvar *object-id*)
-(defvar *table-objects* NIL)
+(defparameter *perception-objects* NIL)
+(defparameter *running* NIL)
+(defparameter *next-object* NIL)
 
+;;@author Torge Olliges
 (defun execute-grocery()
+  ;;waiting for start signal loop
+  (cram::wait-for-fluent *running*)
 
-  ;;Get into perceive position
-  (llif:call-nav-action -0.8 0.7 -1.5)
+  ;;move to shelf
+  (llif::call-text-to-speech-action "Hello, i am moving to the shelf now please step out of the way.")
+  ;;TODO: move-hsr is not parameterised
+  (comf::move-hsr (llif::prolog-shelf-pose))
+  ;;perceive shelf put objects into knowledge and bulletworld
+  (llif::call-text-to-speech-action "I am perceiving the first shelf now.")
   (llif::call-take-pose-action 2)
+  (setf *perception-objects* (llif::call-robosherlock-pipeline "robocup_shelf_0"))
+  (llif::insert-knowledge-objects *perception-objects*)
+  (grocery::spawn-btr-objects *perception-objects*)
 
-  ;;perceive
-  (setq *perceptionData* (llif::call-robosherlock-pipeline))
+  (llif::call-text-to-speech-action "I am perceiving the second shelf now.")
+  (llif::call-take-pose-action 2)
+  (setf *perception-objects* (llif::call-robosherlock-pipeline "robocup_shelf_1"))
+  (llif::insert-knowledge-objects *perception-objects*)
+  (grocery::spawn-btr-objects *perception-objects*)
 
-  ;;Get into grasp position
+  (llif::call-text-to-speech-action "I am perceiving the third shelf now.")
+  (llif::call-take-pose-action 3)
+  (setf *perception-objects* (llif::call-robosherlock-pipeline "robocup_shelf_2"))
+  (llif::insert-knowledge-objects *perception-objects*)
+  (grocery::spawn-btr-objects *perception-objects*)
+
+  (llif::call-text-to-speech "I am done perceiving the shelf now.")
+  ;;back to base position
   (llif::call-take-pose-action 1)
-  (llif::call-nav-action -0.8 0.7 3.15)
 
-  ;;Insert found objects into knowledge base
-	(llif::insert-knowledge-objects *perceptionData*)
+  ;;move to table
+  (llif::call-text-to-speech-action "Hello, i am moving to the table now please step out of the way.")
+  ;;TODO: move-hsr is not parameterised
+  (comf::move-hsr (llif::prolog-table-pose))
 
-  ;;Get next object
-  (setq *object-id* (comf:next-object))
+  ;;perceiving the table
+  (llif::call-text-to-speech-action "I am perceiving the table now.")
+  (llif::call-take-pose-action 2)
+  (setf *perception-objects* (llif::call-robosherlock-pipeline "robocup_table"))
+  (llif::insert-knowledge-objects *perception-objects*)
+  (grocery::spawn-btr-objects *perception-objects*)
 
-  ;;Grasping object
-  (comf:grasp-object *object-id* 1)
-  
-  ;;(comf:place-object *object-id*)
+  (llif::call-text-to-speech-action "I am done perceiving the table now.")
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;LOOP
+  ;;move to table
+  (llif::call-text-to-speech-action "I am getting into a position to grasp from.")
+  ;;TODO: move-hsr is not parameterised
+  (comf::move-hsr (llif::prolog-table-pose))
+
+  ;;query for next object
+  (setf *next-object* (llif::prolog-next-object))
+  ;;grasp object
+  (comf::grasp-object *next-object* 1)
+
+  ;;move to shelf
+  (llif::call-text-to-speech-action "Hello, i am moving to the shelf now please step out of the way.")
+  ;;TODO: move-hsr is not parameterised
+  (comf::move-hsr (llif::prolog-shelf-pose))
+
+  ;;check for correct position depending on other objects in shelf
+  (stf *object-goal-pose* (llif::prolog-object-goal-pose *next-object*))
+
+  ;;place object in shelf
+  (comf::place-object *object-goal-pose*)
+
+  ;;back to base position
+  (llif::call-take-pose-action 1)
+
+  ;;query for knowledge if objects left
+  ;;(if (= (length (llif::prolog-table-objects)) 0) (;;continue loop) (;;stop loop))
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;LOOP
   )
-
-
-(defun next-object ()
-    "get the next Object to grasp"
-  (setf *table-objects* (llif:prolog-table-objects))
-  (first *table-objects*))
