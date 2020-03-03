878c22f878bcb2202f0f1776403d9aec700bebd5
tomlehmk <tomlehmk@uni-bremen.de>
2020-03-03 20:51:12 +0100

    edit execute-grocery

--- grocery_demo/src/execute-grocery.lisp
+++ grocery_demo/src/execute-grocery.lisp
@@ -1,17 +1,20 @@
 (in-package :grocery)
 (defparameter *perception-objects* NIL)
-(defparameter *running* NIL)
+;;(defparameter *running* NIL)
+(defparameter *no-objects* (cram-language:make-fluent :name :objects) NIL)
 (defparameter *next-object* NIL)
 
-;;@author Torge Olliges
+;;@author Torge Olliges, Tom-Eric Lehmkuhl
 (defun execute-grocery()
   ;;waiting for start signal loop
-  (cram::wait-for-fluent *running*)
+  (cram-language:pursue
+      (cram-language:wait-for *state-fluent*)
+      (loop do
+        (cram-language:sleep 0.1)))
 
   ;;move to shelf
   (llif::call-text-to-speech-action "Hello, i am moving to the shelf now please step out of the way.")
-  ;;TODO: move-hsr is not parameterised
-  (comf::move-hsr (llif::prolog-shelf-pose))
+  (comf::move-to-shelf)
   ;;perceive shelf put objects into knowledge and bulletworld
   (llif::call-text-to-speech-action "I am perceiving the first shelf now.")
   (llif::call-take-pose-action 2)
@@ -37,8 +40,7 @@
 
   ;;move to table
   (llif::call-text-to-speech-action "Hello, i am moving to the table now please step out of the way.")
-  ;;TODO: move-hsr is not parameterised
-  (comf::move-hsr (llif::prolog-table-pose))
+  (comf::move-to-table)
 
   ;;perceiving the table
   (llif::call-text-to-speech-action "I am perceiving the table now.")
@@ -46,35 +48,37 @@
   (setf *perception-objects* (llif::call-robosherlock-pipeline "robocup_table"))
   (llif::insert-knowledge-objects *perception-objects*)
   (grocery::spawn-btr-objects *perception-objects*)
-
   (llif::call-text-to-speech-action "I am done perceiving the table now.")
 
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;LOOP
-  ;;move to table
-  (llif::call-text-to-speech-action "I am getting into a position to grasp from.")
-  ;;TODO: move-hsr is not parameterised
-  (comf::move-hsr (llif::prolog-table-pose))
+  (cram-language:pursue
+      (cram-language:wait-for *objects*)
+      (loop do
+        ;;move to table
+        (llif::call-text-to-speech-action "I am getting into a position to grasp from.")
+        (comf::move-to-table)
 
-  ;;query for next object
-  (setf *next-object* (llif::prolog-next-object))
-  ;;grasp object
-  (comf::grasp-object *next-object* 1)
+        ;;query for next object
+        (setf *next-object* (llif::prolog-next-object))
+        ;;grasp object
+        (llif::call-text-to-speech-action "I'm going to grasp the object now.")
+        (comf::grasp-object *next-object* 1)
+        (llif::call-text-to-speech-action "I have grapsed the object")
 
-  ;;move to shelf
-  (llif::call-text-to-speech-action "Hello, i am moving to the shelf now please step out of the way.")
-  ;;TODO: move-hsr is not parameterised
-  (comf::move-hsr (llif::prolog-shelf-pose))
+        ;;move to shelf
+        (llif::call-text-to-speech-action "Hello, i am moving to the shelf now please step out of the way.")
+        (comf::move-to-shelf)
 
-  ;;check for correct position depending on other objects in shelf
-  (stf *object-goal-pose* (llif::prolog-object-goal-pose *next-object*))
+        ;;check for correct position depending on other objects in shelf
+        (stf *object-goal-pose* (llif::prolog-object-goal-pose *next-object*))
 
-  ;;place object in shelf
-  (comf::place-object *object-goal-pose*)
+        ;;place object in shelf
+        (llif::call-text-to-speech-action "I'm going to place the object in the shelf now.")
+        (comf::place-object *object-goal-pose*)
+        (llif::call-text-to-speech-action "I have placed the object now.")
 
-  ;;back to base position
-  (llif::call-take-pose-action 1)
+        ;;back to base position
+        (llif::call-take-pose-action 1)))
 
-  ;;query for knowledge if objects left
-  ;;(if (= (length (llif::prolog-table-objects)) 0) (;;continue loop) (;;stop loop))
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;LOOP
-  )
+        ;;query for knowledge if objects left
+        ;;(if (= (length (llif::prolog-table-objects)) 0) (set *no-objects* true)
+)
