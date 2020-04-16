4b3922d946c6442ec87afece0c9c4a906a9191e1
tolliges <torge1995@hotmail.de>
2020-04-17 00:14:09 +0200

    added a parameter to get-confident-objects to be able to change the threshold value for filtering

--- common_functions/src/perception-functions.lisp
+++ common_functions/src/perception-functions.lisp
@@ -1,18 +1,18 @@
 (in-package :comf)
 
 ;;@author Torge Olliges
-(defun get-confident-objects(perception-msg)
+(defun get-confident-objects(perception-msg, &optional (threshold 0.5))
     (roslisp::with-fields (detectiondata) perception-msg
         (delete-if 
             (lambda (object) 
             (roslisp::with-fields (confidence_class confidence_shape confidence_color) object 
                 ;;TODO: why is this commented out? does it not work? if it doesnt fix it and uncomment
                 ;;(roslisp:ros-info (confident) "~%class: = ~F~% shape = ~F~% color: ~F" confidence_class confidence_shape confidence_color) 
-                (if (>= confidence_class 0.5) 
+                (if (>= confidence_class threshold) 
                   NIL
-                  (if (>= confidence_shape 0.5) 
+                  (if (>= confidence_shape threshold) 
                       NIL
-                      (if (>= confidence_color 0.5) 
+                      (if (>= confidence_color threshold) 
                           NIL
                       T)))))
             detectiondata)
