d358d18e2e601c939b79ab77269c35043f3910f3
PhilippKatze <phklein@uni-bremen.de>
2020-05-05 15:39:27 +0200

    fixed merge problems

--- grocery_demo/src/execute-grocery.lisp
+++ grocery_demo/src/execute-grocery.lisp
@@ -8,23 +8,7 @@
 (defparameter *place-object-result* NIL)
 (defparameter *grasping-retries* 0)
 
-<<<<<<< Updated upstream
-=======
-;;@author Tom-Eric Lehmkuhl
-(defun execute-grocery-nlp()
-    ;;TODO: uncomment when NLP works
-  ;;waiting for start signal loop
-  (comf::with-hsr-process-modules
-    (cram-language:pursue
-      (cram-language:wait-for low-level-interfacing::*state-fluent*)
-        (loop do
-          (cram-language:sleep 0.1)))
 
-  (cram-language:pursue
-    (cram-language:wait-for (pulsed llif::*state-fluent*))
-    (grocery:execute-grocery))))
-
->>>>>>> Stashed changes
 ;;@author Torge Olliges, Tom-Eric Lehmkuhl
 (defun execute-grocery()
   ;;(comf::with-hsr-process-modules
