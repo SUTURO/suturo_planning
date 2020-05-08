86134c8b7cf961f8f13350fcf15c6a8e03be466d
tomlehmk <tomlehmk@uni-bremen.de>
2020-05-08 14:41:26 +0200

    add authors indentations

--- low_level_interfacing/src/knowledge-insertion-client.lisp
+++ low_level_interfacing/src/knowledge-insertion-client.lisp
@@ -32,31 +32,41 @@
                        :timeout *robosherlock-action-timeout*
                        :result-timeout *robosherlock-action-timeout*)))
 
+;; @author Tom-Eric Lehmkuhl
 (defun knowledge-insertion-test ()
-(let* ((position (roslisp:make-msg "geometry_msgs/Point" (x) 2 (y) 4 (z) 0))
-  (orientation (roslisp:make-msg "geometry_msgs/Quaternion" (x) 0 (y) 1 (z) 0 (w) 1))
-  (pose (roslisp:make-msg "geometry_msgs/Pose" (position) position (orientation) orientation))
-  (header (roslisp:make-msg "std_msgs/Header" (frame_id) "/frame"))
-  (objectmessage (roslisp:make-msg "suturo_perception_msgs/ObjectDetectionData"
-      (name) "test-object"
-      (obj_class) "test-ckass"
-      (confidence_class) 1.0
-      (shape) 1
-      (confidence_shape) 0.8
-      (pose) (roslisp:make-msg "geometry_msgs/PoseStamped"
-        (header) header
-        (pose) pose)
-      (width) 1.3
-      (height) 5.7
-      (depth) 0.2
-      (color) (roslisp:make-msg "std_msgs/ColorRGBA"
-        (r) 1
-        (g) 1
-        (b) 0
-        (a) 0)
-      (confidence_color) 0.6
-      (region) "somewhere"))
-
-  (detection-data (roslisp:make-msg "knowledge_msgs/StoreObjectInfoGoal"
-      (detectionData) (make-array 1 :initial-contents '((objectmessage))))))
+  "Insertion test with dummy message.
+   Not working yet. Probably wrong message type"
+  (roslisp:ros-info (knowledge-insertion-client)
+                    "Insert dummy object to knowledge")
+  (let* ((position (roslisp:make-msg "geometry_msgs/Point" (x) 2 (y) 4 (z) 0))
+         (orientation (roslisp:make-msg "geometry_msgs/Quaternion"
+                                      (x) 0 (y) 1 (z) 0 (w) 1))
+         (pose (roslisp:make-msg "geometry_msgs/Pose" (position) position
+                               (orientation) orientation))
+         (header (roslisp:make-msg "std_msgs/Header" (frame_id) "/frame"))
+         (objectmessage (roslisp:make-msg
+                         "suturo_perception_msgs/ObjectDetectionData"
+                         (no-applicable-method) "test-object"
+                         (obj_class) "test-ckass"
+                         (confidence_class) 1.0
+                         (shape) 1
+                         (confidence_shape) 0.8
+                         (pose) (roslisp:make-msg "geometry_msgs/PoseStamped"
+                                 (header) header
+                                 (pose) pose)
+                         (width) 1.3
+                         (height) 5.7
+                         (depth) 0.2
+                         (color) (roslisp:make-msg "std_msgs/ColorRGBA"
+                                  (r) 1
+                                  (g) 1
+                                  (b) 0
+                                  (a) 0)
+                         (confidence_color) 0.6
+                         (region) "robocup_default"))
+         
+         (detection-data (roslisp:make-msg "knowledge_msgs/StoreObjectInfoGoal"
+                                           (detectionData)
+                                           (make-array 1 :initial-contents
+                                                       '((objectmessage))))))
     (insert-knowledge-objects detection-data)))
