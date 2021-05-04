b9e16951352d8093f0ac3cca98253c8c8b8dea32
Torge Olliges <tolliges@uni-bremen.de>
2021-05-04 13:26:17 +0000

    new execute cleanup plan (wasnt mentioned in the last commit), grasp/place handling should now work properly

--- cleanup_demo/src/execute-cleanup-new.lisp
+++ cleanup_demo/src/execute-cleanup-new.lisp
@@ -38,7 +38,8 @@
           (target-surface (llif::prolog-object-goal next-object)))
         (when (eq next-object 1) (return-from handle-found-objects nil))
         (comf::reachability-check-grasp next-object 1)
-        
+        ;;TODO is the next-object still valid?
+
         (comf::announce-movement-to-surface "future" source-surface)
         (comf::move-to-surface source-surface nil)
 
@@ -54,27 +55,32 @@
 ;;@author Torge Olliges
 (defun grasp-handling (next-object)
     (let ((grasp-action-result (comf::grasp-object next-object 1)))
-                (if (eq grasp-action-result 0)
+        (roslisp::with-fields (error_code)
+                grasp-action-result
+                (if (eq error_code 0)
                     (comf::announce-grasp-action "past" next-object)
-                    ((cpl:with-retry-counters ((grasp-retries 1))
+                    (cpl:with-retry-counters ((grasp-retries 1))
                         (cpl:with-failure-handling
                             (((or 
                                 common-fail:low-level-failure 
                                 cl::simple-error
                                 cl::simple-type-error)
                                 (e)
-                                (comf::announce-grasp-action "failed"  nex-object)
-                                (cpl:do-retry grasping-retry
+                                (comf::announce-grasp-action "failed"  next-object)
+                                (cpl:do-retry grasp-retries
                                     (roslisp:ros-warn (grasp-handling) "~%Failed to grasp the object~%")
                                     (cpl:retry))
                                 (roslisp:ros-warn (going-demo movement-fail) "~%No more retries~%")))
-                            (setf *graspmode* 1)  ;;sets the graspmode should be replaces with the function from knowledge when that is finished
-                            (comf::grasp-object next-object *graspmode*)
-                            (comf::announce-grasp-action "past" next-object)))))))
+                          (setf *grasp-mode* 1)  ;;sets the graspmode should be replaces with the function from knowledge when that is finished
+                          (comf::grasp-object next-object *grasp-mode*)
+                          (comf::announce-grasp-action "past" next-object)))))))
+
 
 ;;@author Torge Olliges
 (defun place-handling (next-object)
     (let ((place-action-result (comf::place-object next-object 1)))
+        (roslisp::with-fields (error_code)
+            place-action-result
                 (if (eq place-action-result 0)
                     (comf::announce-place-action "past" next-object)
                     ((cpl:with-retry-counters ((place-retries 1))
@@ -85,10 +91,13 @@
                                 cl::simple-type-error)
                                 (e)
                                 (comf::announce-place-action "failed"  nex-object)
-                                (cpl:do-retry place-retry
+                                (cpl:do-retry place-retries
                                     (roslisp:ros-warn (place-handling) "~%Failed to grasp the object~%")
                                     (cpl:retry))
                                 (roslisp:ros-warn (going-demo movement-fail) "~%No more retries~%")))
-                            (setf *graspmode* 1)  ;;sets the graspmode should be replaces with the function from knowledge when that is finished
+                            (setf *grasp-mode* 1)  ;;sets the graspmode should be replaces with the function from knowledge when that is finished
                             (comf::place-object next-object *graspmode*)
-                            (comf::announce-place-action "past" next-object)))))))
+                            (if (eq (comf::reachability-check-place next-object *grasp-mode*) 1)
+                                (throw common-fail:low-level-failure)
+                                (comf::place-object next-object *grasp-mode*))
+                            (comf::announce-place-action "past" next-object))))))))
