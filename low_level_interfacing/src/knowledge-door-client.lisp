4d82942f894daf79aa33c609fad821dda7ee16c5
jan <schimpf@uni-bremen.de>
2021-05-20 15:57:30 +0200

    Work on open-door-queries

--- low_level_interfacing/src/knowledge-door-client.lisp
+++ low_level_interfacing/src/knowledge-door-client.lisp
@@ -1,15 +1,29 @@
 (in-package :llif)
 
+;; @author Jan Schimpg
+(defun prolog-get-door-state(door-id)
+  "Gives back the state of that door"
+  (roslisp:ros-info (json-prolog-client) "Getting all doors and their states")
+  (let* ((raw-response (with-safe-prolog
+                         (json-prolog:prolog-simple
+                          (concatenate 'string "get_door_state('"
+                                       door-id
+                                       "',DOORSTATE).") :package :llif))))
+    (if (eq raw-response 1)
+            (roslisp:ros-warn (json-prolog-client)
+                              "Query didn't reach any solution.")
+            (cdr (assoc '?DoorState   (cut:lazy-car raw-response))))))
+
 ;; @author Jan Schimpf
 (defun prolog-get-all-door-states()
   "Gives back an list of the door names and the state"
   (roslisp:ros-info (json-prolog-client) "Getting all doors and their states")
   (let* ((raw-response (with-safe-prolog
-                         (json-prolog:prolog-simple "get_all_door_states(POSES)"  :package :llif))))
+                         (json-prolog:prolog-simple "get_all_door_states(POSES)."  :package :llif))))
       (if (eq raw-response 1)
             (roslisp:ros-warn (json-prolog-client)
                               "Query didn't reach any solution.")
-               (cdr (car (cut:lazy-car raw-response))))))
+               (car (cdr (assoc '?Poses (cut:lazy-car raw-response)))))))
 
 ;; @author Jan Schimpf
 (defun prolog-update-door-state(door-id angle)
@@ -17,11 +31,11 @@
   (roslisp:ros-info (json-prolog-client) "Updating the door state")
   (let* ((raw-response (with-safe-prolog
                          (json-prolog:prolog-simple
-                          (concatenate 'string "update_door_state_dynamic('"door-id"','"angle"')")))))
+                          (concatenate 'string "update_door_state_dynamic('"door-id"',"angle").")))))
         (if (eq raw-response 1)
             (roslisp:ros-warn (json-prolog-client)
                               "Query update_door_state_dynamic didn't reach any solution.")
-            (cut:lazy-car raw-response))))
+            raw-response)))
 
 ;; @author Jan Schimpf
 (defun prolog-get-angle-to-open-door (door-id)
@@ -38,23 +52,27 @@
 (defun prolog-perceiving-pose-of-door (door-id)
  "returns the pose needed to perceive the door"
  (let* ((raw-response (with-safe-prolog
-                         (json-prolog:prolog-simple  (concatenate 'string "perceiving_pose_of_door('"
-                                       door-id "', POSE)")  :package :llif))))
+                        (json-prolog:prolog-simple
+                         (concatenate 'string "perceiving_pose_of_door('"
+                                      door-id
+                                      "', POSE)")  :package :llif))))
         (if (eq raw-response 1)
             (roslisp:ros-warn (json-prolog-client)
                               "Query get_door_state didn't reach any solution.")
-            (cut:lazy-car raw-response))))
+             (cdr (assoc '?Pose (cut:lazy-car raw-response))))))
 
 ;; @author Jan Schimpf
 (defun prolog-manipulating-pose-of-door(door-id)
   "returns the pose from which we can manipulate the door"
  (let* ((raw-response (with-safe-prolog
-                         (json-prolog:prolog-simple  (concatenate 'string "manipulating_pose_of_door('"
-                                       door-id "', POSE)")  :package :llif))))
+                        (json-prolog:prolog-simple
+                         (concatenate 'string "manipulating_pose_of_door('"
+                                      door-id
+                                      "', POSE)")  :package :llif))))
         (if (eq raw-response 1)
             (roslisp:ros-warn (json-prolog-client)
                               "Query get_door_state didn't reach any solution.")
-            (cut:lazy-car raw-response))))  
+             (cdr (assoc '?Pose (cut:lazy-car raw-response))))))
 
 ;; @author Torge Olliges
 (defun prolog-shortest-path-between-rooms (start-room-id target-room-id)
