c96cf573128f67ad3bf98fcc7c5616d9d37f6e1f
jan <schimpf@uni-bremen.de>
2021-06-02 11:51:17 +0200

    small adjustment to open door

--- common_functions/src/high-level-plans.lisp
+++ common_functions/src/high-level-plans.lisp
@@ -138,7 +138,7 @@
 
 ;;@author Jan Schimpf
 ;;Gets the door id as input and move into position to open it, currently only for one door as the position query isn't done yet
-(defun open-roomdoor (door-id)
+(defun open-room-door (door-id)
  (cpl:with-retry-counters ((grasping-retry 2))
     (cpl:with-failure-handling
         (((or common-fail:low-level-failure 
@@ -156,22 +156,26 @@
             "~%No more retries~%")))
 
       ;;this is here for testing purposes only / until it is replaced with the proper querry
-      (comf::get-nav-pose-for-doors (llif::prolog-manipulating-pose-of-door door-id) t)
-      ;; go into percieve position (as manipulation works with its own angle this isn't needed yet)
-
-     
-      ;; insert into knowledge ...
-      
-      ;; query knowledge for ID (manipulation doesn't use this currently ...
+      (let ((knowledge-pose-manipulation (llif::prolog-manipulating-pose-of-door door-id)))
+             (comf::get-nav-pose-for-doors knowledge-pose-manipulation t)
+           
       (let ((knowledge-doorhandle-id (concatenate 'string "iai_kitchen/" (llif::prolog-knowrob-name-to-urdf-link
                                                                           (car (cdr (llif::prolog-perceiving-pose-of-door door-id))))))
-            (knowledge-pose   (car (llif::prolog-perceiving-pose-of-door door-id)))
+            (knowledge-pose-perceiving   (car (llif::prolog-perceiving-pose-of-door door-id)))
             (knowledge-open-door-angle (llif::prolog-get-angle-to-open-door door-id)))
         (llif::call-open-action knowledge-doorhandle-id
                                 knowledge-doorhandle-id
                                 1.35)
         (llif::prolog-update-door-state door-id "1.35")
-        (comf::get-nav-pose-for-doors knowledge-pose t)))))
+        (comf::get-nav-pose-for-doors knowledge-pose-manipulation t)
+        (comf::get-nav-pose-for-doors knowledge-pose-perceiving t))))))
+
+;;@author Jan Schimpf
+(defun open-door-on-the-path (start-room target-room)
+  (loop for door-id in (llif::prolog-shortest-path-between-rooms start-room target-room)
+        do
+           (comf::open-room-door door-id)
+        ))
 
 
 ;;@author Torge Olliges
