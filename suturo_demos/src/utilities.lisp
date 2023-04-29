c5fa104ab63a8b37ea45eb5533b71266c181cc51
suturo1 <you@example.com>
2023-04-29 02:37:09 +0200

    Luca Krohm, bulletworld workaround, knowledge util functions

--- suturo_demos/src/utilities.lisp
+++ suturo_demos/src/utilities.lisp
@@ -22,12 +22,12 @@
 ;;             (print b))
 ;;@author Tede von Knorre, Felix Krause
 (defmacro with-knowledge-result (vars query &body body)
-  `(let ((raw-response (simple-knowledge ,query )))
+  `(let ((raw-response (simple-knowledge ,query (find-package :common-lisp-user))))
      (if (eq raw-response 1)
          (error "Knowledge query failed at the macro with-knowledge-result")
          (unwind-protect
               (let ,(loop for x in vars
-                          collect `(,x (fix-prolog-string (cdr (assoc ',(match-prolog-symbol x) (car raw-response))))))
+                          collect `(,x (fix-prolog-string (cdr (assoc ',(match-prolog-symbol x (find-package :common-lisp-user)) (car raw-response))))))
                 ,@body
                 )
            (json-prolog:finish-query raw-response)))))
@@ -35,9 +35,19 @@
 
 
 ;;@author Tede von Knorre, Felix Krause
-(defun simple-knowledge (query)
+(defun simple-knowledge (query &optional (package *package*))
+  (print package)
   (with-safe-prolog
-    (json-prolog:prolog query)))
+    (json-prolog:prolog query :package package)))
+
+;;@author Felix Krause
+(defun make-pose-stamped-from-knowledge-result (result)
+  (cl-tf:make-pose-stamped
+   (first result) 0.0
+   (cl-tf:make-3d-vector
+    (first (second result)) (second (second result)) (third (second result)))
+   (cl-tf:make-quaternion (first (third result)) (second (third result)) (third (third result)) (fourth (third result))))
+  )
 
 
 ;; TODO add list of possible querys
@@ -98,19 +108,6 @@
 ;;;;;;;;;;;;;;;;;;
 ;; Fake-Designator
 
-(defun check-arm-for-object ()
-  (let ((link
-        (cut:var-value
-          '?link
-          (car
-           (prolog:prolog
-            `(and (rob-int:robot ?rob)
-                  (rob-int:end-effector-link ?rob :left ?link)))))))
-    (car
-     (btr:link-attached-object-names
-     (btr:get-robot-object)
-     link))))
-
 (defgeneric exe-perform-type (action-type &key &allow-other-keys))
 
 (defmethod exe-perform-type ((action-type (eql :picking-up)) &key (arm :left) object)
@@ -134,10 +131,176 @@
 
 (defmethod exe-perform-type ((action-type (eql :placing)) &key (arm :left) pose)
   (let*((obj-name (check-arm-for-object)))
-      (btr-utils:move-object
-         obj-name
-         pose)
-      (cram-occasions-events:on-event
-       (make-instance 'cpoe:object-detached-robot
-                      :arm (list arm)
-                      :object-name obj-name))))
+    (btr-utils:move-object
+     obj-name
+     pose)
+    (cram-occasions-events:on-event
+     (make-instance 'cpoe:object-detached-robot
+                    :arm (list arm)
+                    :object-name obj-name))))
+
+(defmethod exe-perform-type ((action-type (eql :opening)) &key object (distance 1.7))
+    (cond
+      ((search "right" object)  (let ((?object-pose (knowledge-get-right-handle-pose)))
+                                  (exe:perform
+                                   (desig:an action
+                                             (type looking)
+                                             (target (desig:a location (pose ?object-pose)))))
+                                  (sleep 1)
+                                  (let ((dist (* distance 1)))
+                                    (btr:set-robot-state-from-joints
+                                     `((,object ,dist))
+                                     (btr:get-environment-object)))))
+       
+      ((search "left" object)  (let ((?object-pose (knowledge-get-left-handle-pose)))
+                                 (exe:perform
+                                  (desig:an action
+                                            (type looking)
+                                            (target (desig:a location (pose ?object-pose)))))
+                                 (sleep 1)
+                                 (let ((dist (* distance -1)))
+                                   (btr:set-robot-state-from-joints
+                                    `((,object ,dist))
+                                    (btr:get-environment-object)))))
+      
+      ((search "origin" object)  (let ((?object-pose (knowledge-get-entrance-handle-pose)))
+                                  (exe:perform
+                                   (desig:an action
+                                             (type looking)
+                                             (target (desig:a location (pose ?object-pose)))))
+                                   (sleep 1)
+                                   (let ((dist (if (> distance 0)
+                                                    0
+                                                    distance)))
+                                     (btr:set-robot-state-from-joints
+                                      `((,object ,dist))
+                                      (btr:get-environment-object)))))
+      (t (error "Please enter a valid openable object"))))
+
+(defmethod exe-perform-type ((action-type (eql :closing)) &key object)
+  (cond
+      ((search "right" object)  (let ((?object-pose (knowledge-get-right-handle-pose)))
+                                  (exe:perform
+                                   (desig:an action
+                                             (type looking)
+                                             (target (desig:a location (pose ?object-pose)))))
+                                  (sleep 1)
+                                  (btr:set-robot-state-from-joints
+                                   `((,object 0))
+                                   (btr:get-environment-object))))
+       
+      ((search "left" object)  (let ((?object-pose (knowledge-get-left-handle-pose)))
+                                 (exe:perform
+                                  (desig:an action
+                                            (type looking)
+                                            (target (desig:a location (pose ?object-pose)))))
+                                 (sleep 1)
+                                 (btr:set-robot-state-from-joints
+                                  `((,object 0))
+                                  (btr:get-environment-object))))
+      
+      ((search "origin" object)  (let ((?object-pose (knowledge-get-entrance-handle-pose)))
+                                   (exe:perform
+                                    (desig:an action
+                                              (type looking)
+                                              (target (desig:a location (pose ?object-pose)))))
+                                   (sleep 1)
+                                     (btr:set-robot-state-from-joints
+                                      `((,object -1.4))
+                                      (btr:get-environment-object))))
+      (t (error "Please enter a valid openable object"))))
+
+(defmethod exe-perform-type ((action-type (eql :reaching)) &key (arm :left) ?object)
+        (exe:perform
+         (desig:an action
+                   (type reaching)
+                   (object ?object)
+                   )))
+
+;;;;;;;;;;;;;;
+;; BTR-Utility
+
+;; use to quickly test navigation poses 
+(defun move-hsr-debug (?pose)
+  (urdf-proj:with-simulated-robot
+    (exe:perform
+     (desig:an action
+               (type going)
+               (target (desig:a location (pose ?pose)))))))
+
+;; example for how to spawn objects
+(defun spawn-pringles()
+  (btr:add-object btr:*current-bullet-world* :cylinder-item 'cylinder-1
+                    '((1.5 -1.5 0.78) (0 0 0 1))
+                    :mass 0.2 :size (cl-transforms:make-3d-vector 0.03 0.03 0.08)
+                    :item-type :pringles))
+
+(defun move-obj (obj-name pose)
+  (btr-utils:move-object
+   obj-name
+   pose))
+
+(defun check-arm-for-object ()
+  (let ((link
+        (cut:var-value
+          '?link
+          (car
+           (prolog:prolog
+            `(and (rob-int:robot ?rob)
+                  (rob-int:end-effector-link ?rob :left ?link)))))))
+    (car
+     (btr:link-attached-object-names
+     (btr:get-robot-object)
+     link))))
+
+;;;;;;;;;;;;
+;; Knowledge
+
+;; if you are using a different map, you might need to adjust the strings to reference the proper link
+
+(defun knowledge-get-left-handle-pose()
+  (cl-tf:pose->pose-stamped
+   cram-tf:*fixed-frame*
+   0 
+   (btr:link-pose (btr:get-environment-object) "shelf:shelf:shelf_door_left:shelf_link_handle")))
+
+(defun knowledge-get-right-handle-pose()
+  (cl-tf:pose->pose-stamped
+   cram-tf:*fixed-frame*
+   0 
+   (btr:link-pose (btr:get-environment-object) "shelf:shelf:shelf_door_right:shelf_link_handle")))
+
+(defun knowledge-get-entrance-handle-pose()
+  (cl-tf:pose->pose-stamped
+   cram-tf:*fixed-frame*
+   0 
+   (btr:link-pose (btr:get-environment-object) "iai_kitchen:living_room:door_handle_inside")))
+
+
+;; those are the joints that can be assigned different values to open/close objects, for example doors
+(defun knowledge-get-shelf-left()
+  "shelf:shelf:shelf_door_left:shelf_joint")
+
+(defun knowledge-get-shelf-right()
+  "shelf:shelf:shelf_door_right:shelf_joint")
+
+(defun knowledge-get-entrance()
+  "iai_kitchen:living_room:door_origin_revolute_joint")
+
+(defun knowledge-get-looking-pose(obj-name)
+  (cl-tf:pose->pose-stamped
+                   cram-tf:*fixed-frame*
+                   0 
+                   (btr:object-pose obj-name)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun round-for-knowrob (v &optional (n 10))
+  (declare (type float v)
+           (type (integer 0) n))
+  (setf v (coerce v 'single-float))
+  (let ((10^-n (expt 10 (- n))))
+     (* (fround v 10^-n)
+               10^-n)))
+
